7f130597dc761b574aac57645f574e32c809dccf
golamkibriya <kamalmia5052@gmail.com>
2022-10-26 03:48:16 +0600

    site is responsive

--- src/component/NotFound.js
+++ src/component/NotFound.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function NotFound() {
   return (
     <div>
-        <h1>404 </h1>
-        <h2>pages donot found!!!!!</h2>
+        <h1>404 Error...</h1>
+        <h2>Pages Not Found!!!!!</h2>
     </div>
   )
 }
