3185eaa54f9b0133f2e6f5ec6999c76645b692f4
golamkibriya <kamalmia5052@gmail.com>
2022-10-26 14:34:53 +0600

    faq already done

--- src/component/FAQ/Faq.js
+++ src/component/FAQ/Faq.js
@@ -1,8 +1,81 @@
-import React from 'react'
-import './Faq.css'
+import React from "react";
+import "./Faq.css";
 
 export default function Faq() {
   return (
-    <div>Faq</div>
-  )
+    <div>
+      <div>
+        <h5> 1. Who is the father of Computers?</h5>
+
+        <p> a) James Gosling b) Charles Babbage</p>
+        <p> c) Dennis Ritchie d) Bjarne Stroustrup</p>
+      </div>
+
+      <div>
+        <h5>
+          2. Which of the following is the correct abbreviation of COMPUTER?
+        </h5>
+        <p>
+          a) Commonly Occupied Machines Used in Technical and Educational
+          Research b) Commonly Operated Machines Used in Technical and
+          Environmental Research
+        </p>
+        <p>
+          a) Commonly Occupied Machines Used in Technical and Educational
+          Research b) Commonly Operated Machines Used in Technical and
+          Environmental Research
+        </p>
+      </div>
+
+      <div>
+        <h5>3. What is the full form of CPU?</h5>
+        <p>a) Computer Processing Unit b) Computer Principle Unit</p>
+        <p>c) Central Processing Unit d) Control Processing Unit</p>
+      </div>
+      <div>
+        <h5>
+          4. Which of the following language does the computer understand?
+        </h5>
+        <p>
+          a) Computer understands only C Language b) Computer understands only
+          Assembly Language
+        </p>
+        <p>
+          c) Computer understands only Binary Language d) Computer understands
+          only BASIC
+        </p>
+      </div>
+
+      <div>
+        <h5>
+          5. Which of the following computer language is written in binary codes
+          only?
+        </h5>
+        <p>a) pascal b) machine language</p>
+        <p>c) C d) C#</p>
+      </div>
+
+      <div>
+        <h5>6. Which of the following is the brain of the computer?</h5>
+        <p>a) Central Processing Unit b) Memory</p>
+        <p>c) Arithmetic and Logic unit d) Control unit</p>
+      </div>
+
+      <div>
+        <h5>
+          7. Which of the following is not a characteristic of a computer?
+        </h5>
+        <p>a) Versatility b) Accuracy</p>
+        <p>c) Diligence d) I.Q.</p>
+      </div>
+
+      <div>
+        <h5>
+          8. Which of the following is the smallest unit of data in a computer?
+        </h5>
+        <p>a) Bit b) KB</p>
+        <p>c) Nibble d) Byte</p>
+      </div>
+    </div>
+  );
 }
