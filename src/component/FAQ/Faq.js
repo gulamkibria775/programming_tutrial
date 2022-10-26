2c95add0a20a064da4574c8efbbb8aeb24feb4b6
golamkibriya <kamalmia5052@gmail.com>
2022-10-26 14:42:43 +0600

    blog and faq router are created

--- src/component/FAQ/Faq.js
+++ src/component/FAQ/Faq.js
@@ -7,8 +7,8 @@ export default function Faq() {
       <div>
         <h5> 1. Who is the father of Computers?</h5>
 
-        <p> a) James Gosling b) Charles Babbage</p>
-        <p> c) Dennis Ritchie d) Bjarne Stroustrup</p>
+        <p> a) James Gosling <br/> b) Charles Babbage</p>
+        <p> c) Dennis Ritchie <br/> d) Bjarne Stroustrup</p>
       </div>
 
       <div>
@@ -17,31 +17,31 @@ export default function Faq() {
         </h5>
         <p>
           a) Commonly Occupied Machines Used in Technical and Educational
-          Research b) Commonly Operated Machines Used in Technical and
+          Research <br/> b) Commonly Operated Machines Used in Technical and
           Environmental Research
         </p>
         <p>
-          a) Commonly Occupied Machines Used in Technical and Educational
-          Research b) Commonly Operated Machines Used in Technical and
-          Environmental Research
+          c) Commonly Oriented Machines Used in Technical and Educational
+          Research <br/> d) Commonly Operated Machines Used in Technical and
+          Educational Research
         </p>
       </div>
 
       <div>
         <h5>3. What is the full form of CPU?</h5>
-        <p>a) Computer Processing Unit b) Computer Principle Unit</p>
-        <p>c) Central Processing Unit d) Control Processing Unit</p>
+        <p>a) Computer Processing Unit <br/> b) Computer Principle Unit</p>
+        <p>c) Central Processing Unit <br/> d) Control Processing Unit</p>
       </div>
       <div>
         <h5>
           4. Which of the following language does the computer understand?
         </h5>
         <p>
-          a) Computer understands only C Language b) Computer understands only
+          a) Computer understands only C Language  <br/>b) Computer understands only
           Assembly Language
         </p>
         <p>
-          c) Computer understands only Binary Language d) Computer understands
+          c) Computer understands only Binary Language  <br/>d) Computer understands
           only BASIC
         </p>
       </div>
@@ -51,30 +51,30 @@ export default function Faq() {
           5. Which of the following computer language is written in binary codes
           only?
         </h5>
-        <p>a) pascal b) machine language</p>
-        <p>c) C d) C#</p>
+        <p>a) pascal<br/> b) machine language</p>
+        <p>c) C  <br/>d) C#</p>
       </div>
 
       <div>
         <h5>6. Which of the following is the brain of the computer?</h5>
-        <p>a) Central Processing Unit b) Memory</p>
-        <p>c) Arithmetic and Logic unit d) Control unit</p>
+        <p>a) Central Processing Unit <br/>b) Memory</p>
+        <p>c) Arithmetic and Logic unit<br/> d) Control unit</p>
       </div>
 
       <div>
         <h5>
           7. Which of the following is not a characteristic of a computer?
         </h5>
-        <p>a) Versatility b) Accuracy</p>
-        <p>c) Diligence d) I.Q.</p>
+        <p>a) Versatility<br/> b) Accuracy</p>
+        <p>c) Diligence <br/>d) I.Q.</p>
       </div>
 
       <div>
         <h5>
           8. Which of the following is the smallest unit of data in a computer?
         </h5>
-        <p>a) Bit b) KB</p>
-        <p>c) Nibble d) Byte</p>
+        <p>a) Bit<br/> b) KB</p>
+        <p>c) Nibble<br/> d) Byte</p>
       </div>
     </div>
   );
