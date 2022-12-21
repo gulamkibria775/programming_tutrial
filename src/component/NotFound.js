a4c9e45d9b2c6c491f0608313be7dbcc78789185
golamkibriya <kamalmia5052@gmail.com>
2022-12-21 23:01:06 +0600

    deletle unessary file

--- src/component/NotFound.js
+++ src/component/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
   return (
     <div>
-        <h1>404 Error...</h1>
+        <h1>404 Error...!!!</h1>
         <h2>Pages Not Found!!!!!</h2>
     </div>
   )
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDXUvob3oUMg0KNz4hWSu4CZeoyuhjAFLs",
   authDomain: "computer-program-4c723.firebaseapp.com",
